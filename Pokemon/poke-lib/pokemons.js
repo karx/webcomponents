8d39380fc3782ebf1c6dd51b3c0ed13448d8b3ad
Kartik <kartik.arora1214@gmail.com>
2020-05-07 16:08:46 +0530

    Fix path to pokemon images
    https://github.com/fanzeyi/pokemon.json/pull/16

--- Pokemon/poke-lib/pokemons.js
+++ Pokemon/poke-lib/pokemons.js
@@ -6,9 +6,13 @@ export function nameOfPokemonFromId(id) {
     return pokedex.en_pokedex[id-1].name.english;
 }
 
+export function numberOfPokemonFromId(id) {
+    return ("00" + id).slice(-3);
+}
+
 export function pokemonImageSourceFromId(id) {
     var base_str = '00000' + id;
-    var imageName = base_str.substr(base_str.length - 3, base_str) + nameOfPokemonFromId(id);
+    var imageName = numberOfPokemonFromId(id);
     return `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/thumbnails/${imageName}.png`;
 }
 
